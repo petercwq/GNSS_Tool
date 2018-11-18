3776ea34d5718b3b8d5940dabc20aacae407f8d6
Peter Chen <peter_chen@trimble.com>
2018-11-18 17:57:24 +0800

    First commit

--- GNSS_Tool/MainActivity.cs
+++ GNSS_Tool/MainActivity.cs
@@ -1,17 +1,34 @@
-﻿using System;
+﻿using Android;
 using Android.App;
+using Android.Content.PM;
 using Android.OS;
-using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Support.V4.App;
+using Android.Support.V4.Content;
 using Android.Support.V7.App;
 using Android.Views;
 using Android.Widget;
+using NmeaParser.Nmea;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace GNSS_Tool
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private Dictionary<string, string> devices = new Dictionary<string, string>();
+        private Queue<NmeaMessage> messages = new Queue<NmeaMessage>(100);
+
+        private NmeaParser.NmeaDevice listener;
+        private TextView tvOutput, tvLat, tvLon, tvAlt;
+        // private bool launched;
+        private Android.Bluetooth.BluetoothSocket socket;
+
+        private static NmeaParser.NmeaDevice SystemGPS;
+
+        private FloatingActionButton fab;
 
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,8 +38,27 @@ namespace GNSS_Tool
             Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
             SetSupportActionBar(toolbar);
 
-            FloatingActionButton fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
+            fab = FindViewById<FloatingActionButton>(Resource.Id.fab);
             fab.Click += FabOnClick;
+
+            tvOutput = FindViewById<TextView>(Resource.Id.output);
+            tvLon = FindViewById<TextView>(Resource.Id.longitude);
+            tvLat = FindViewById<TextView>(Resource.Id.latitude);
+            tvAlt = FindViewById<TextView>(Resource.Id.altitude);
+
+            devices.Add("System GPS", null);
+            var devicePicker = FindViewById<Spinner>(Resource.Id.device_picker);
+            Java.Util.UUID SERIAL_UUID = Java.Util.UUID.FromString("00001101-0000-1000-8000-00805F9B34FB");
+            var adapter = Android.Bluetooth.BluetoothAdapter.DefaultAdapter;
+            if (adapter != null)
+            {
+                foreach (var d in adapter.BondedDevices.Where(d => d.GetUuids().Where(t => t.Uuid.ToString().Equals("00001101-0000-1000-8000-00805F9B34FB", StringComparison.InvariantCultureIgnoreCase)).Any()))
+                {
+                    devices[d.Name + " " + d.Address] = d.Address;
+                }
+            }
+            devicePicker.Adapter = new ArrayAdapter<string>(this, Android.Resource.Layout.SimpleSpinnerDropDownItem, devices.Keys.ToArray());
+            devicePicker.SetSelection(0);
         }
 
         public override bool OnCreateOptionsMenu(IMenu menu)
@@ -44,10 +80,127 @@ namespace GNSS_Tool
 
         private void FabOnClick(object sender, EventArgs eventArgs)
         {
-            View view = (View) sender;
-            Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
-                .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+            //View view = (View) sender;
+            //Snackbar.Make(view, "Replace with your own action", Snackbar.LengthLong)
+            //    .SetAction("Action", (Android.Views.View.IOnClickListener)null).Show();
+
+            fab.Enabled = false;
+            if (listener?.IsOpen == true)
+            {
+                Stop();
+                fab.SetImageResource(Android.Resource.Drawable.IcMediaPlay);
+            }
+            else
+            {
+                Start();
+                fab.SetImageResource(Android.Resource.Drawable.IcMediaPause);
+            }
+            fab.Enabled = true;
         }
-	}
+
+        private void Stop()
+        {
+            if (listener != null)
+                listener.MessageReceived -= Listener_MessageReceived;
+            socket?.Close();
+            socket?.Dispose();
+            socket = null;
+            listener?.CloseAsync();
+            listener = null;
+        }
+
+        private async void Start()
+        {
+            var devicePicker = FindViewById<Spinner>(Resource.Id.device_picker);
+            var id = devicePicker.SelectedItem.ToString();
+            var btAddress = devices[id];
+            if (btAddress == null)
+            {
+                if (ContextCompat.CheckSelfPermission(this, Manifest.Permission.AccessFineLocation) != Permission.Granted)
+                {
+                    ActivityCompat.RequestPermissions(this, new[] { Manifest.Permission.AccessFineLocation }, 1000);
+                    return;
+                }
+
+                if (SystemGPS == null)
+                    SystemGPS = new NmeaParser.SystemNmeaDevice();
+
+                listener = SystemGPS;
+            }
+            else //Bluetooth
+            {
+                try
+                {
+                    tvOutput.Text = "Opening bluetooth...";
+                    var adapter = Android.Bluetooth.BluetoothAdapter.DefaultAdapter;
+                    var bt = Android.Bluetooth.BluetoothAdapter.DefaultAdapter.GetRemoteDevice(btAddress);
+                    Java.Util.UUID SERIAL_UUID = Java.Util.UUID.FromString("00001101-0000-1000-8000-00805F9B34FB"); //UUID for Serial Device Service
+                    socket = bt.CreateRfcommSocketToServiceRecord(SERIAL_UUID);
+                    try
+                    {
+                        await socket.ConnectAsync();
+                    }
+                    catch (Java.IO.IOException)
+                    {
+                        // This sometimes fails. Use fallback approach to open socket
+                        // Based on https://stackoverflow.com/a/41627149
+                        socket.Dispose();
+                        var m = bt.Class.GetMethod("createRfcommSocket", new Java.Lang.Class[] { Java.Lang.Integer.Type });
+                        socket = m.Invoke(bt, new Java.Lang.Object[] { 1 }) as Android.Bluetooth.BluetoothSocket;
+                        socket.Connect();
+                    }
+                    listener = new NmeaParser.StreamDevice(socket.InputStream);
+                }
+                catch (System.Exception ex)
+                {
+                    socket?.Dispose();
+                    socket = null;
+                    tvOutput.Text += "\nError opening Bluetooth device:\n" + ex.Message;
+                }
+            }
+
+            if (listener != null)
+            {
+                listener.MessageReceived += Listener_MessageReceived;
+                tvOutput.Text += "\nOpening device...";
+                await listener.OpenAsync();
+                tvOutput.Text += "\nConnected!";
+            }
+        }
+
+
+        protected override void OnDestroy()
+        {
+            Stop();
+            base.OnDestroy();
+        }
+
+        protected override void OnResume()
+        {
+            base.OnResume();
+            // if it was resumed by the GPS permissions dialog
+            //Start();
+        }
+
+        private void Listener_MessageReceived(object sender, NmeaParser.NmeaMessageReceivedEventArgs e)
+        {
+            var message = e.Message;
+            RunOnUiThread(() =>
+            {
+                if (messages.Count == 100) messages.Dequeue();
+                messages.Enqueue(message);
+                tvOutput.Text = string.Join("\n", messages.Reverse().Select(n => n.ToString()));
+                if (message is Rmc rmc)
+                {
+                    tvLat.Text = "Latitude = " + rmc.Latitude.ToString("0.0000000");
+                    tvLon.Text = "Longitude = " + rmc.Longitude.ToString("0.0000000");
+                }
+                else if (message is Gga gga)
+                {
+                    tvAlt.Text = "Altitude = " + gga.Altitude.ToString() + " " + gga.AltitudeUnits.ToString();
+                }
+            });
+        }
+    }
 }
 
---
+++ GNSS_Tool/Parser/NmeaDevice.cs
@@ -0,0 +1,323 @@
+﻿﻿//
+// Copyright (c) 2014 Morten Nielsen
+//
+// Licensed under the Microsoft Public License (Ms-PL) (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://opensource.org/licenses/Ms-PL.html
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+//
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
+
+namespace NmeaParser
+{
+	/// <summary>
+	/// A generic abstract NMEA device
+	/// </summary>
+	public abstract class NmeaDevice : IDisposable
+	{
+		private object m_lockObject = new object();
+		private string m_message = "";
+		private Stream m_stream;
+		private System.Threading.CancellationTokenSource m_cts;
+		private TaskCompletionSource<bool> m_closeTask;
+        private bool m_isOpening;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="NmeaDevice"/> class.
+        /// </summary>
+        protected NmeaDevice()
+		{
+		}
+
+        /// <summary>
+		/// Opens the device connection.
+		/// </summary>
+		/// <returns></returns>
+		public async Task OpenAsync()
+		{
+			lock (m_lockObject)
+			{
+				if (IsOpen || m_isOpening) return;
+                m_isOpening = true;
+			}
+			m_cts = new System.Threading.CancellationTokenSource();
+			m_stream = await OpenStreamAsync();
+			StartParser();
+			MultiPartMessageCache.Clear();
+            lock (m_lockObject)
+            {
+                IsOpen = true;
+                m_isOpening = false;
+            }
+        }
+
+        [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Performance", "CA1804:RemoveUnusedLocals", MessageId = "_")]
+		private void StartParser()
+		{
+			var token = m_cts.Token;
+			System.Diagnostics.Debug.WriteLine("Starting parser...");
+			var _ = Task.Run(async () =>
+			{
+				byte[] buffer = new byte[1024];
+				while (!token.IsCancellationRequested)
+				{
+					int readCount = 0;
+					try
+					{
+						readCount = await ReadAsync(buffer, 0, 1024, token).ConfigureAwait(false);
+					}
+					catch { }
+					if (token.IsCancellationRequested)
+						break;
+					if (readCount > 0)
+					{
+						OnData(buffer.Take(readCount).ToArray());
+					}
+					await Task.Delay(50);
+				}
+				if (m_closeTask != null)
+					m_closeTask.SetResult(true);
+			});
+		}
+
+        /// <summary>
+        /// Performs a read operation of the stream
+        /// </summary>
+        /// <param name="buffer">The buffer to write the data into.</param>
+        /// <param name="offset">The byte offset in buffer at which to begin writing data from the stream.</param>
+        /// <param name="count">The maximum number of bytes to read.</param>
+        /// <param name="cancellationToken">The token to monitor for cancellation requests. The default value is System.Threading.CancellationToken.None.</param>
+        /// <returns>
+        /// A task that represents the asynchronous read operation. The value of the TResult
+        /// parameter contains the total number of bytes read into the buffer. The result
+        /// value can be less than the number of bytes requested if the number of bytes currently
+        /// available is less than the requested number, or it can be 0 (zero) if the end
+        /// of the stream has been reached.
+        /// </returns>
+        protected virtual Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+        {
+            if (m_stream == null)
+                return Task.FromResult(0);
+            return m_stream.ReadAsync(buffer, 0, 1024, cancellationToken);
+        }
+
+        /// <summary>
+        /// Creates the stream the NmeaDevice is working on top off.
+        /// </summary>
+        /// <returns></returns>
+        protected abstract Task<Stream> OpenStreamAsync();
+
+		/// <summary>
+		/// Closes the device.
+		/// </summary>
+		/// <returns></returns>
+		public async Task CloseAsync()
+		{
+			if (m_cts != null)
+			{
+				m_closeTask = new TaskCompletionSource<bool>();
+				if (m_cts != null)
+					m_cts.Cancel();
+				m_cts = null;
+			}
+			await m_closeTask.Task;
+			await CloseStreamAsync(m_stream);
+			MultiPartMessageCache.Clear();
+			m_stream = null;
+            lock (m_lockObject)
+            {
+                m_isOpening = false;
+                IsOpen = false;
+            }
+		}
+		/// <summary>
+		/// Closes the stream the NmeaDevice is working on top off.
+		/// </summary>
+		/// <param name="stream">The stream.</param>
+		/// <returns></returns>
+		protected abstract Task CloseStreamAsync(Stream stream);
+
+		private void OnData(byte[] data)
+		{
+			var nmea = System.Text.Encoding.UTF8.GetString(data, 0, data.Length);
+			List<string> lines = new List<string>();
+			lock (m_lockObject)
+			{
+				m_message += nmea;
+
+				var lineEnd = m_message.IndexOf("\n", StringComparison.Ordinal);
+				while (lineEnd > -1)
+				{
+					string line = m_message.Substring(0, lineEnd).Trim();
+					m_message = m_message.Substring(lineEnd + 1);
+					if (!string.IsNullOrEmpty(line))
+						lines.Add(line);
+					lineEnd = m_message.IndexOf("\n", StringComparison.Ordinal);
+				}
+			}
+			foreach(var line in lines)
+				ProcessMessage(line);
+		}
+
+		[System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Design", "CA1031:DoNotCatchGeneralExceptionTypes", Justification="Must silently handle invalid/corrupt input")]
+		private void ProcessMessage(string p)
+		{
+			try
+			{
+				var msg = NmeaParser.Nmea.NmeaMessage.Parse(p);
+				if (msg != null)
+					OnMessageReceived(msg);
+			}
+			catch { }
+		}
+
+		private void OnMessageReceived(Nmea.NmeaMessage msg)
+		{
+			var args = new NmeaMessageReceivedEventArgs(msg);
+			if (msg is IMultiPartMessage multi)
+			{
+				args.IsMultipart = true;
+				if (MultiPartMessageCache.ContainsKey(msg.MessageType))
+				{
+					var dic = MultiPartMessageCache[msg.MessageType];
+					if (dic.ContainsKey(multi.MessageNumber - 1) && !dic.ContainsKey(multi.MessageNumber))
+					{
+						dic[multi.MessageNumber] = msg;
+					}
+					else //Something is out of order. Clear cache
+						MultiPartMessageCache.Remove(msg.MessageType);
+				}
+				else if (multi.MessageNumber == 1)
+				{
+					MultiPartMessageCache[msg.MessageType] = new Dictionary<int, Nmea.NmeaMessage>(multi.TotalMessages);
+					MultiPartMessageCache[msg.MessageType][1] = msg;
+				}
+				if (MultiPartMessageCache.ContainsKey(msg.MessageType))
+				{
+					var dic = MultiPartMessageCache[msg.MessageType];
+					if (dic.Count == multi.TotalMessages) //We have a full list
+					{
+						MultiPartMessageCache.Remove(msg.MessageType);
+						args.MessageParts = dic.Values.ToArray();
+                    }
+                }
+			}
+
+			if (MessageReceived != null)
+			{
+				MessageReceived(this, args);
+			}
+		}
+
+		private Dictionary<string, Dictionary<int, Nmea.NmeaMessage>> MultiPartMessageCache
+			= new Dictionary<string,Dictionary<int,Nmea.NmeaMessage>>();
+
+		/// <summary>
+		/// Occurs when an NMEA message is received.
+		/// </summary>
+		public event EventHandler<NmeaMessageReceivedEventArgs> MessageReceived;
+
+		/// <summary>
+		/// Releases unmanaged and - optionally - managed resources.
+		/// </summary>
+		public void Dispose()
+		{
+			Dispose(true);
+			GC.SuppressFinalize(this);
+		}
+
+		/// <summary>
+		/// Releases unmanaged and - optionally - managed resources.
+		/// </summary>
+		/// <param name="disposing"><c>true</c> to release both managed and unmanaged resources; <c>false</c> to release only unmanaged resources.</param>
+		protected virtual void Dispose(bool disposing)
+		{
+			if (m_stream != null)
+			{
+				if (m_cts != null)
+				{
+					m_cts.Cancel();
+					m_cts = null;
+				}
+				CloseStreamAsync(m_stream);
+				if (disposing && m_stream != null)
+					m_stream.Dispose();
+				m_stream = null;
+			}
+		}
+
+		/// <summary>
+		/// Gets a value indicating whether this device is open.
+		/// </summary>
+		/// <value>
+		///   <c>true</c> if this instance is open; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsOpen { get; private set; }
+
+        /// <summary>
+        /// Gets a value indicating whether this device supports writing
+        /// </summary>
+        /// <seealso cref="WriteAsync(byte[], int, int)"/>
+        public virtual bool CanWrite { get => false; }
+
+        /// <summary>
+        /// Writes to the device stream. Useful for transmitting RTCM corrections to the device
+        /// Check the <see cref="CanWrite"/> property before calling this method.
+        /// </summary>
+		/// <param name="buffer">The byte array that contains the data to write to the port.</param>
+		/// <param name="offset">The zero-based byte offset in the buffer parameter at which to begin copying 
+		/// bytes to the port.</param>
+		/// <param name="length">The number of bytes to write.</param>
+        /// <returns>Task</returns>
+        /// <seealso cref="CanWrite"/>
+        public virtual Task WriteAsync(byte[] buffer, int offset, int length)
+        {
+            throw new NotSupportedException();
+        }
+	}
+
+	/// <summary>
+	/// Event argument for the <see cref="NmeaDevice.MessageReceived" />
+	/// </summary>
+	public sealed class NmeaMessageReceivedEventArgs : EventArgs
+	{
+		internal NmeaMessageReceivedEventArgs(Nmea.NmeaMessage message) {
+			Message = message;
+		}
+		/// <summary>
+		/// Gets the nmea message.
+		/// </summary>
+		/// <value>
+		/// The nmea message.
+		/// </value>
+		public Nmea.NmeaMessage Message { get; private set; }
+		/// <summary>
+		/// Gets a value indicating whether this instance is a multi part message.
+		/// </summary>
+		/// <value>
+		/// <c>true</c> if this instance is multi part; otherwise, <c>false</c>.
+		/// </value>
+		public bool IsMultipart { get; internal set; }
+		/// <summary>
+		/// Gets the message parts if this is a multi-part message and all message parts has been received.
+		/// </summary>
+		/// <value>
+		/// The message parts.
+		/// </value>
+		public IReadOnlyList<Nmea.NmeaMessage> MessageParts { get; internal set; }
+	}
+}
